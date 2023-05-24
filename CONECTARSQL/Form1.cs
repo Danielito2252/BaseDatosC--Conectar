7d7a0a5c1b9ec172e2ad9a7980b091eacccff697
Danielito2252 <hbarriosb4@miumg.edu.gt>
2023-05-24 00:46:14 -0600

    Se agrego un comentario en texto1

---
+++ CONECTARSQL/Form1.cs
@@ -0,0 +1,40 @@
+//Agregar Librerias de conexion
+
+using System.Data.SqlClient;
+using System.Data;
+
+namespace CONECTARSQL
+{
+    public partial class Form1 : Form
+    {
+        //Conexion a la BD
+        SqlConnection conn = new SqlConnection(@"Data Source=localhost;Initial Catalog=Northwind;Integrated Security=True");
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            cargarDatos();
+        }
+        private void cargarDatos()
+        {
+            SqlDataAdapter da= new SqlDataAdapter("select * from Products",conn);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            this .dataGridView1.DataSource = dt;    
+        }
+    }
+}
